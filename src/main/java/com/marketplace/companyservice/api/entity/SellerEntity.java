c2ac0cc3d9a937fd4fe1ce6be0ef66108642ccfc
SvetlanaMaeva <mystik20009@gmail.com>
2023-04-14 13:47:20 +0300

    добавлена миграция и entity для таблицы seller

---
+++ src/main/java/com/marketplace/companyservice/api/entity/SellerEntity.java
@@ -0,0 +1,40 @@
+package com.marketplace.companyservice.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Сущность  продовца.
+ */
+@Entity
+@Table(name = "seller", schema = "company")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SellerEntity {
+    @Id
+    @Column
+    @NonNull
+    private UUID id;
+    @Column
+    @NonNull
+    private String name;
+    @Column
+    @NonNull
+    private String surname;
+    @Column
+    @NonNull
+    private String email;
+    @Column(name = "registration_date")
+    @NonNull
+    private OffsetDateTime registrationDate;
+}
