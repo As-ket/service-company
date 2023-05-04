1febeb39326c2e030f26e06b19a241abbe08069d
Pashkin Evgenii <e.n.pashkin@gmail.com>
2023-05-04 16:53:47 +0300

    Решены конфликты

--- src/main/java/com/marketplace/companyservice/api/util/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/marketplace/companyservice/api/util/handlers/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.marketplace.companyservice.api.util.handlers;
 
+import com.marketplace.companyservice.api.util.exceptions.InvalidSizeDocAttachException;
+import com.marketplace.companyservice.api.util.exceptions.InvalidTypeDocAttachException;
 import com.marketplace.companyservice.api.util.exceptions.CompanyNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -7,15 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-<<<<<<< HEAD
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-=======
->>>>>>> 445b073 (Внесены правки.)
 
 /**
  * Перехват исключений
  */
-<<<<<<< HEAD
 
 @Slf4j
 @ControllerAdvice
@@ -29,14 +27,12 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
     @ExceptionHandler(value = {InvalidTypeDocAttachException.class})
     public ResponseEntity<String> invalidTypeDocAttach(RuntimeException ex) {
-=======
-@Slf4j
-@ControllerAdvice
-public class RestResponseEntityExceptionHandler {
+        log.info(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(value = {CompanyNotFoundException.class})
     public ResponseEntity<String> companyNotFound(RuntimeException ex) {
->>>>>>> 445b073 (Внесены правки.)
         log.info(ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
